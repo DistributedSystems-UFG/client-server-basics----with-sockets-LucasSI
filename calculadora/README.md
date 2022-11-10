d93dc76820ede0f44184aebc3e0e0a791cee4beb
LUCAS SILVA IRINEU <lucasirineu@hotmail.com>
2022-11-09 23:48:04 -0300

    Add files via upload

---
+++ calculadora/README.md
@@ -0,0 +1,7 @@
+# ClientServerBasics
+Starter code for the basic client-server assignment
+
+
+Atividade 1 - Criação de uma calculadora para apresentar interação entre cliente - servidor onde o cliente passa uma equação sendo ela uma soma, uma subtração uma multiplicação ou uma divisão e recebe devolta a resposta.
+
+https://drive.google.com/file/d/1h1jqhD45bv6UJ-KMVxVHFvGKx_LdU_1R/view?usp=share_link video do funcionamento feito em localhost pois não consegui configurar o aws para fazer a comunicação entre duas instancias.
---
+++ calculadora/server.py
@@ -0,0 +1,39 @@
+from socket  import *
+from constCS import * #-
+
+s = socket(AF_INET, SOCK_STREAM) 
+s.bind((HOST, PORT))  #-
+s.listen(1)           #-
+(conn, addr) = s.accept()  # returns new socket and addr. client 
+while True:                # forever
+  data = conn.recv(1024)   # receive data from client
+  if not data: break       # stop if client stopped
+  print(bytes.decode(data))
+  valor = bytes.decode(data)
+  for i in range(len(valor)):
+                 if valor[i] == '+' or valor[i] == '-' or valor[i] == '*' or valor[i] == '/':
+                   operacao = i
+  if valor[operacao] == '+':
+    num1 = int(valor[:operacao])
+    num2 = int(valor[operacao+1:])
+    total = num1 + num2
+    conn.send(str.encode(str(total))) # return sum of two numbers sent from client
+  elif valor[operacao] == '-':
+    num1 = int(valor[:operacao])
+    num2 = int(valor[operacao+1:])
+    total = num1 - num2
+    conn.send(str.encode(str(total))) # return substraction of two numbers sent by client
+  elif valor[operacao] == '*':
+    num1 = int(valor[:operacao])
+    num2 = int(valor[operacao+1:])
+    total = num1 * num2
+    conn.send(str.encode(str(total))) # return multiplication of two numbers sent by client
+  elif valor[operacao] == '/':
+    num1 = int(valor[:operacao])
+    num2 = int(valor[operacao+1:])
+    total = num1 / num2
+    conn.send(str.encode(str(total))) # return division of two numbers sent by client
+  else:
+    conn.send(str.encode('Não foi possivel realizar a equação')) # return division of two numbers sent by client
+    break
+conn.close()               # close the connection
